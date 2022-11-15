01e8300dc79b144bc4a462ffac0bc10fd49ac5a4
nikita <nikitamozzilkin@gmail.com>
2022-11-16 01:34:06 +0400

    Mobile version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-menu'),
+        menuItem = document.querySelectorAll('.nav-menu__list-item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+        })
+    })
+})
+
+
+
+
+
+
+
     $(document).ready(function(){
     $("a[href*='#']").on("click", function(e){
         var anchor = $(this);
